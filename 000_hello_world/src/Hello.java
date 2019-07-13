739f85a045c4846c2771a68c48092e404d316444
Brandon Monier <brandon.monier@gmail.com>
2019-07-13 07:50:49 -0400

    Add header and additional objectives

--- 000_hello_world/src/Hello.java
+++ 000_hello_world/src/Hello.java
@@ -1,8 +1,33 @@
+//-------------------------------------------------------------------
+// Lesson Name:   001 - Hello, world...
+// Author:        Brandon Monier
+// Created:       2019-07-13
+// Last Modified: 2019-07-13
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+// Detailed Purpose:
+//    Learn basic class structure and variables.
+//-------------------------------------------------------------------
+
 public class Hello {
 
     public static void main(String[] args ) {
 
-        System.out.println("Hello, World! Let's learn some Java.");
+        // Obligatory
+        System.out.println("Hello, World! Let's learn some Java.\n");
+
+        // Lets learn about variables
+        int myFirstNumber = 5;
+        System.out.println("This is my first integer............ " + myFirstNumber);
+
+        // We can modify variables with operators and other ints and int variables
+        int mySecondNumber = myFirstNumber * 5;
+        System.out.println("This is my modified first integer... " + mySecondNumber);
+
+        int myThirdNumber = myFirstNumber + mySecondNumber;
+        System.out.println("This is my third integer............ " + myThirdNumber);
 
     }
+
 }
