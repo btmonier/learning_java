ff0dbc3534fc9d28d51262427421e0b6a5645899
Brandon Monier <brandon.monier@gmail.com>
2019-07-13 16:17:51 -0400

    Add lesson description

--- 002_float_and_double/src/com/btmonier/Main.java
+++ 002_float_and_double/src/com/btmonier/Main.java
@@ -1,8 +1,22 @@
+//-------------------------------------------------------------------
+// Lesson Name:   002 - Floats and doubles...
+// Author:        Brandon Monier
+// Created:       2019-07-13
+// Last Modified: 2019-07-13
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+// Detailed Purpose:
+//    Learn about primitive data types:
+//      * float
+//      * double
+//-------------------------------------------------------------------
+
 package com.btmonier;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        
     }
 }
