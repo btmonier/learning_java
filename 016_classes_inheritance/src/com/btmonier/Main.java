d07f6ba01be5cc966ee4d26b3db0ca34576de817
Brandon <brandon.monier@gmail.com>
2020-07-11 18:10:05 -0400

    Initial commit

---
+++ 016_classes_inheritance/src/com/btmonier/Main.java
@@ -0,0 +1,21 @@
+//-------------------------------------------------------------------
+// Lesson Name:   016 - Class inheritance
+// Author:        Brandon Monier
+// Created:       2020-07-05
+// Last Modified: 2020-07-05
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+// Detailed Purpose:
+//   In this lesson, we will go over the concepts of inheritance and
+//   how this applies to classes in Java.
+//-------------------------------------------------------------------
+
+package com.btmonier;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
