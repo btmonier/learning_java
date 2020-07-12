33442468198e6de32b3102ea62126935b16f599c
Brandon <brandon.monier@gmail.com>
2020-07-12 16:38:49 -0400

    Add output

--- 016_classes_inheritance/src/com/btmonier/Main.java
+++ 016_classes_inheritance/src/com/btmonier/Main.java
@@ -1,8 +1,8 @@
 //-------------------------------------------------------------------
 // Lesson Name:   016 - Class inheritance
 // Author:        Brandon Monier
-// Created:       2020-07-05
-// Last Modified: 2020-07-05
+// Created:       2020-07-12
+// Last Modified: 2020-07-12
 //-------------------------------------------------------------------
 
 //-------------------------------------------------------------------
@@ -16,6 +16,10 @@ package com.btmonier;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+        Dog dog = new Dog("Labrador", 8, 20, 2, 4, 1, 20, "short");
+//        dog.eat();
+        dog.walk();
+//        dog.run();
     }
 }
